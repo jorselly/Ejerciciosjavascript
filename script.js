37eb0e083fcea1ecc6b7e69257bd6f6fc8fbbec5
jorsellydiaz0gmail.com <jorsellydiaz0@gmail.com>
2023-12-20 10:37:02 +0100

    Ejercicio 2

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ console.log("hola soy jorselly")
 function sumaNumeros(numero1, numero2) {
     return numero1 + numero2;
 }
-function sumaNumeros(numero1, numero2) {
-    return numero1 + numero2;
-}
+
 
 // Ejercicio 1
 const resultado = sumaNumeros(8, 8);
 console.log(resultado);
+
+// Ejercicio 2
+function restaNumeros(numero1, numero2) {
+    return numero1 - numero2;
+}
+const resultadoResta = restaNumeros(8, 3);
+console.log(resultadoResta); 
+
+
 
 
 
