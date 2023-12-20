76515105318d93d373daf3c258ce4bdbece6aa1a
jorsellydiaz0gmail.com <jorsellydiaz0@gmail.com>
2023-12-20 12:55:32 +0100

    Ejercicio 3

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ function restaNumeros(numero1, numero2) {
 const resultadoResta = restaNumeros(8, 3);
 console.log(resultadoResta); 
 
+//Ejercicio 3
+function multiplicaNumeros(numero1, numero2) {
+    return numero1 * numero2;
+}
+const resultadoMultiplicacion = multiplicaNumeros(8, 8);
+console.log(resultadoMultiplicacion);
 
 
 
